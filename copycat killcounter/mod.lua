315cbe1018c26264b485f77176a00695d9c0d1d6
act2498 <117634708+act2498@users.noreply.github.com>
2023-01-20 21:13:30 +0800

    Update mod.lua

--- copycat killcounter/mod.lua
+++ copycat killcounter/mod.lua
@@ -1,3 +1,12 @@
+--set up someting
+local mp = ModPath
+local op = mp..'a.ogg'
+if blt.xaudio and io.file_is_readable(ThisOGGPath) then
+	blt.xaudio.setup()
+else
+	return
+end
+
 local o_func = PlayerManager.on_killshot
 local s_k = 0
 local p_k = 0
@@ -8,12 +17,16 @@ function PlayerManager:on_killshot(killed_unit, variant, headshot, weapon_id)
 		local selection_index = equipped_unit and equipped_unit:base() and equipped_unit:base():selection_index() or 0
 		if selection_index == 1 then
 			s_k = s_k % 10 + 1
-			managers.chat:_receive_message(1, "SYSTEM", 'secondary_kills='..s_k, tweak_data.system_chat_color)	
+			if s_k == 10 then
+				XAudio.UnitSource:new(XAudio.PLAYER, XAudio.Buffer:new(op)):set_volume(1)
+			end
 			return
 		end
 		if selection_index == 2 then
 			p_k = p_k % 10 + 1
-			managers.chat:_receive_message(1, "SYSTEM", 'primary_kills='..p_k, tweak_data.system_chat_color)
+			if p_k == 10 then
+				XAudio.UnitSource:new(XAudio.PLAYER, XAudio.Buffer:new(op)):set_volume(1)
+			end
 		end
 	end
 end
