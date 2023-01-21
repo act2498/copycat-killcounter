a0460e38077dc35eae3a876972882062bdc9ca9f
act2498 <117634708+act2498@users.noreply.github.com>
2023-01-21 23:22:02 +0800

    Update mod.lua

--- copycat killcounter/mod.lua
+++ copycat killcounter/mod.lua
@@ -9,22 +9,21 @@ if blt.xaudio and io.file_is_readable(op) then
 else
 	return
 end
-
 Hooks:PostHook(PlayerManager, "on_killshot", hook1, function(self,killed_unit, variant, headshot, weapon_id)
-if self._has_secondary_reload_primary or self._has_primary_reload_secondary then
-	local equipped_unit = self:get_current_state()._equipped_unit
-	local selection_index = equipped_unit and equipped_unit:base() and equipped_unit:base():selection_index() or 0
-	if selection_index == 1 then
-		s_k = s_k % 10 + 1
-		if s_k == 10 then
-			XAudio.UnitSource:new(XAudio.PLAYER, XAudio.Buffer:new(op)):set_volume(1)
+	if self._has_secondary_reload_primary or self._has_primary_reload_secondary then
+		local equipped_unit = self:get_current_state()._equipped_unit
+		local selection_index = equipped_unit and equipped_unit:base() and equipped_unit:base():selection_index() or 0
+		if selection_index == 1 then
+			s_k = s_k % 10 + 1
+			if s_k == 10 then
+				XAudio.UnitSource:new(XAudio.PLAYER, XAudio.Buffer:new(op)):set_volume(1)
+			return
 		end
-		return
-	end
-	if selection_index == 2 then
-		p_k = p_k % 10 + 1
-		if p_k == 10 then
-			XAudio.UnitSource:new(XAudio.PLAYER, XAudio.Buffer:new(op)):set_volume(1)
+		if selection_index == 2 then
+			p_k = p_k % 10 + 1
+			if p_k == 10 then
+				XAudio.UnitSource:new(XAudio.PLAYER, XAudio.Buffer:new(op)):set_volume(1)
+			end
 		end
 	end
 end)
