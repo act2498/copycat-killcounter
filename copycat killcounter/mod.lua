f75f59d4d9249a425b87ebc9a9cf65103311035c
act2498 <117634708+act2498@users.noreply.github.com>
2023-01-21 10:32:10 +0800

    Update mod.lua

--- copycat killcounter/mod.lua
+++ copycat killcounter/mod.lua
@@ -1,15 +1,15 @@
 --set up someting
 local mp = ModPath
 local op = mp..'a.ogg'
+local o_func = PlayerManager.on_killshot
+local s_k = 0
+local p_k = 0
 if blt.xaudio and io.file_is_readable(op) then
 	blt.xaudio.setup()
 else
 	return
 end
 
-local o_func = PlayerManager.on_killshot
-local s_k = 0
-local p_k = 0
 function PlayerManager:on_killshot(killed_unit, variant, headshot, weapon_id)
 	o_func(self,killed_unit, variant, headshot, weapon_id)
 	if self._has_secondary_reload_primary or self._has_primary_reload_secondary then
